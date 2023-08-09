0b8cd4d8a3025de2069ae52467a8b02c20dc6c7d
akash1kumar20 <akashkumar2101sm@gmail.com>
2023-08-09 11:14:14 +0530

    Authentication Added

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,23 +1,37 @@
 import classes from "./Counter.module.css";
+import { counterActions } from "../store/index";
 import { useSelector, useDispatch } from "react-redux";
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector((state) => state.counter);
-  const toggleCounterHandler = () => {};
+  const counter = useSelector((state) => state.counter.counter);
+  //telling react that we want to dive into counter slice and next counter is the value.
+  const show = useSelector((state) => state.counter.showCounter);
+
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch(counterActions.increment());
+  };
+  const increaseHandler = () => {
+    dispatch(counterActions.increase(5));
+    //value pass as the argument is stored as the payload by the redux-toolkit
   };
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
-
+  const resetValue = () => {
+    dispatch(counterActions.reset());
+  };
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter());
+  };
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
-        <button onClick={incrementHandler}>IncrementBy5</button>
-        <button onClick={decrementHandler}>Decremenenby5</button>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>IncrementBy5</button>
+        <button onClick={decrementHandler}>Decremenent</button>
+        <button onClick={resetValue}>Reset</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
